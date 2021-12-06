04b0411f7bdd8caa2874f51cc6635ab63170c209
Cwiekala Michal <cwiekala.michal@gmail.com>
2021-12-06 21:10:44 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 // vite.config.js
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+const { resolve } = require('path');
 
-module.exports = defineConfig({
+module.exports = {
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about', 'index.html'),
-        privacy: resolve(__dirname, 'privacy', 'index.html'),
-        start: resolve(__dirname, 'start', 'index.html'),
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            about: resolve(__dirname, 'src/about/index.html')
       }
     }
   }
-})
+}
