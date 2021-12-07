8a2dcd1751f5268b619149aeaf86bcb273134b34
Cwiekala Michal <cwiekala.michal@gmail.com>
2021-12-07 06:55:52 +0100

    Updates

--- assets/about.aee00636.js
+++
@@ -1,1 +0,0 @@
-import"./style.17613865.js";document.querySelector("#app").innerHTML="\n  <h1>JS dla about!</h1>\n";
---
+++ assets/privacy.e46f44d8.js
@@ -0,0 +1,1 @@
+import"./style.e816881a.js";document.querySelector("#app").innerHTML="\n  <h1>JS dla privacy!!</h1>\n";
